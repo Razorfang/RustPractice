3e658c0940189f5d1d79a72f524dd38e622da1e6
Jamie Getty <jamie.getty@mimomax.com>
2021-01-02 20:07:29 +1300

    Use iterators.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -11,32 +11,32 @@ pub struct Config
 
 impl Config
 {
-    pub fn new(args: &[String]) -> Result<Config, &'static str>
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str>
     {
-        if args.len() < 3
+        args.next(); // Ignore name of program
+
+        let query = match args.next()
         {
-            return Err("Not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string!"),
+        };
+
+        let filename = match args.next()
+        {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename!"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {query, filename, case_sensitive})
     }
 
-    pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>
-    {
-      let mut results = Vec::new();
-
-      for line in contents.lines()
-      {
-         if line.contains(query)
-         {
-               results.push(line);
-         }
-      }
-  
-      results
+    pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+      contents
+          .lines()
+          .filter(|line| line.contains(query))
+          .collect()
     }
 
 
